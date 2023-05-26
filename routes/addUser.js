b7afc793f2de0548b7cd1f409f406c7bb98fefc1
Shivam Karn <shivam3511@gmail.com>
2023-05-27 03:13:22 +0530

    added node-fetch

--- routes/addUser.js
+++ routes/addUser.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const fetch = require('node-fetch')
 const User = require('../models/User');
 
 router.post('/addUser', async (req, res) => {
--- routes/findCompany.js
+++ routes/findCompany.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const fetch = require('node-fetch')
 const verify = require('../middleware/verify.js');
 
 
